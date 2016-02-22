d3beaf88919ac053406a167b6ee165503dce8657
sanket <sanketnaik69@gmail.com>
2016-02-23 00:16:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sanket/myappportfolio/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.sanket.myappportfolio;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b1 = (Button)findViewById(R.id.button1);
+        Button b2 = (Button)findViewById(R.id.button2);
+        Button b3 = (Button)findViewById(R.id.button3);
+        Button b4 = (Button)findViewById(R.id.button4);
+        Button b5 = (Button)findViewById(R.id.button5);
+        Button b6 = (Button)findViewById(R.id.button6);
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
+        b4.setOnClickListener(this);
+        b5.setOnClickListener(this);
+        b6.setOnClickListener(this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId())
+        {
+            case R.id.button1:
+                Toast.makeText(getApplicationContext(),"This button will launch my Spotify App!",Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.button2:
+                Toast.makeText(getApplicationContext(),"This button will launch my Scores App!",Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.button3:
+                Toast.makeText(getApplicationContext(),"This button will launch my Library App!",Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.button4:
+                Toast.makeText(getApplicationContext(),"This button will launch my BuildItBigger App!",Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.button5:
+                Toast.makeText(getApplicationContext(),"This button will launch my XYZReader App!",Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.button6:
+                Toast.makeText(getApplicationContext(),"This button will launch my Capstone App!",Toast.LENGTH_LONG).show();
+                break;
+        }
+    }
+}
